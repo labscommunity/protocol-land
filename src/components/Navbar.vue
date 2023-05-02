5dd4cb025303f2b4814e37a53b20a25b189bdf49
Tate Berenbaum <tateflyer1@gmail.com>
2023-05-02 12:21:56 -1000

    feat: routing and initialized hero

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+<nav class="navbar" role="navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    </a>
+  </div>
+
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Get Started</strong>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+/* a {
+  color: transparent !important;
+} */
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
+import "../node_modules/bulma/css/bulma.min.css";
+import router from "./router";
 
-createApp(App).mount("#app");
+createApp(App).use(router).mount("#app");
