8d4d858c1569e56c49a92bd9cb9f6f0b0988d95b
Sai Kranthi <iamsaikranthi@gmail.com>
2023-08-07 20:13:12 +0530

    refactor: make othent modal text medium size

--- src/components/LoginModal/index.tsx
+++ src/components/LoginModal/index.tsx
@@ -60,7 +60,7 @@ export default function LoginModal({ setIsOpen, isOpen }: LoginModalProps) {
                     <div className="cursor-pointer w-full rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[2px]">
                       <div className="p-3 flex gap-2 rounded-md h-full w-full items-center justify-center bg-[rgba(38,38,44,1)] back">
                         <SVG src={OthentLogo} width={24} height={24} />
-                        <h1 className="text-xl text-[whitesmoke]">Othent</h1>
+                        <h1 className="text-xl text-[whitesmoke] font-medium">Othent</h1>
                       </div>
                     </div>
                   </div>
