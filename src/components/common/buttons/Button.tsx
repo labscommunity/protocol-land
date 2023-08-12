0181c3b2b95be035c8fdf1d9c052654cf5097205
Sai Kranthi <iamsaikranthi@gmail.com>
2023-08-12 13:56:17 +0530

    refactor: layout and poly button

--- src/components/common/buttons/Button.tsx
+++ src/components/common/buttons/Button.tsx
@@ -6,7 +6,7 @@ import { ButtonProps } from './types'
 const VARIANTS = {
   gradient:
     'bg-[linear-gradient(90deg,rgba(170,64,255,1)_10%,rgba(169,69,246,1)_16%,rgba(223,91,216,1)_88%)] text-base tracking-wide text-white',
-  outline: 'text-indigo-700 hover:text-indigo-800 hover:border-b border-indigo-800 !px-0 !pb-1',
+  outline: 'text-[#4388f6] hover:bg-[#4388f6] hover:text-white border-[1.2px] border-[#4388f6]',
   ghost: 'hover:bg-[#c6dcff] text-[#4388f6] rounded-[8px]',
   link: 'hover:border-b border-[#4388f6] !px-0 !pb-1 text-[#4388f6]',
   solid: 'bg-[#4388f6] text-base tracking-wide text-white'
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,16 +1,24 @@
+import clsx from 'clsx'
 import React from 'react'
 
 import LeftBottomImg from '@/assets/images/bg/left_bottom.png'
+import RightTopImg from '@/assets/images/bg/right_top.png'
 import Navbar from '@/components/Navbar'
 
-export default function AppLayout({ children }: { children: React.JSX.Element }) {
-  const style = { '--image-url': `url(${LeftBottomImg})` } as React.CSSProperties
+const style = {
+  '--bg-left-btm-img-url': `url(${LeftBottomImg})`,
+  '--bg-right-top-img-url': `url(${RightTopImg})`
+} as React.CSSProperties
+const afterClasses =
+  'after:content-[""] after:bg-[image:var(--bg-left-btm-img-url)] after:bg-no-repeat after:bg-left-top after:left-0 after:bottom-0 after:absolute after:w-[472px] after:h-[354px]'
+const beforeClasses =
+  'before:content-[""] before:z-[-1] before:bg-[image:var(--bg-right-top-img-url)] before:bg-no-repeat before:bg-left-top before:right-0 before:top-0 before:absolute before:w-[587px] before:h-[705px]'
+const layoutClasses =
+  'z-[1] relative flex flex-col h-screen bg-[linear-gradient(135deg,rgba(215,230,255,1)_0%,rgba(243,219,246,1)_50%,rgba(240,220,247,1)_55%,rgba(217,227,255,1)_100%)]'
 
+export default function AppLayout({ children }: { children: React.JSX.Element }) {
   return (
-    <div
-      style={style}
-      className="flex flex-col before:content-[''] before:bg-[image:var(--image-url)] before:bg-no-repeat before:bg-left-top before:left-0 before:bottom-0 before:absolute before:w-[472px] before:h-[354px] h-screen bg-[linear-gradient(135deg,rgba(215,230,255,1)_0%,rgba(243,219,246,1)_50%,rgba(240,220,247,1)_55%,rgba(217,227,255,1)_100%)]"
-    >
+    <div style={style} className={clsx(layoutClasses, beforeClasses, afterClasses)}>
       <Navbar />
       {children}
     </div>
