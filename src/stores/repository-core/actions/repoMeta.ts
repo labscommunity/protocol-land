cba697c8194532e40cc47177bb5de4b289b1634f
Sai Kranthi <iamsaikranthi@gmail.com>
2023-11-02 10:46:57 +0530

    fix: allow public repos to load without connection

--- src/stores/repository-core/actions/repoMeta.ts
+++ src/stores/repository-core/actions/repoMeta.ts
@@ -1,14 +1,9 @@
-import { InjectedArweaveSigner } from 'warp-contracts-plugin-signature'
-
 import { CONTRACT_TX_ID } from '@/helpers/constants'
 import getWarpContract from '@/helpers/getWrapContract'
 import { Repo, WarpReadState } from '@/types/repository'
 // Repo Meta
 
 export const getRepositoryMetaFromContract = async (id: string): Promise<{ result: Repo }> => {
-  const userSigner = new InjectedArweaveSigner(window.arweaveWallet)
-  await userSigner.setPublicKey()
-
   const contract = getWarpContract(CONTRACT_TX_ID)
 
   return contract.viewState({
