cebc7d10d752f332b80ae57c162684b77538d18c
Sai Kranthi <iamsaikranthi@gmail.com>
2023-11-06 00:41:35 +0530

    refactor: hardcode GA tracking id

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,3 +1,2 @@
 export const CONTRACT_TX_ID = 'w5ZU15Y2cLzZlu3jewauIlnzbKw-OAxbN9G5TbuuiDQ'
-export const VITE_OTHENT_API_KEY = import.meta.env.VITE_OTHENT_API_KEY
-export const VITE_GA_TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID
+export const VITE_GA_TRACKING_ID = "G-L433HSR0D0"
