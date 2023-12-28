20422e9b671c732de47e6a17837327308671d24c
Pawan Paudel <pawanpaudel93@gmail.com>
2023-12-28 22:05:15 +0545

    feat: Initial setup for edit file

--- src/pages/repository/components/tabs/code-tab/index.tsx
+++ src/pages/repository/components/tabs/code-tab/index.tsx
@@ -1,8 +1,13 @@
+import { EditorState } from '@codemirror/state'
+import { EditorView } from '@codemirror/view'
 import { langs } from '@uiw/codemirror-extensions-langs'
 import { githubLight } from '@uiw/codemirror-theme-github'
 import CodeMirror from '@uiw/react-codemirror'
+import clsx from 'clsx'
 import React from 'react'
-import { FiArrowLeft, FiCode, FiEdit3 } from 'react-icons/fi'
+import CodeMirrorMerge from 'react-codemirror-merge'
+import { FiArrowLeft, FiCode } from 'react-icons/fi'
+import { MdOutlineEdit } from 'react-icons/md'
 
 import { Button } from '@/components/common/buttons'
 import { trackGoogleAnalyticsEvent } from '@/helpers/google-analytics'
@@ -29,7 +34,9 @@ export default function CodeTab({ repoName = '', id = '' }: Props) {
     state.repoCoreActions.isContributor,
     state.branchState.currentBranch
   ])
-  const [fileContent, setFileContent] = React.useState('')
+  const [isEditMode, setIsEditMode] = React.useState(false)
+  const [isPreviewMode, setIsPreviewMode] = React.useState(false)
+  const [fileContent, setFileContent] = React.useState({ original: '', modified: '' })
   const [filename, setFilename] = React.useState('')
 
   const { repoCommitsG, fetchFirstCommit } = useCommit()
@@ -66,13 +73,24 @@ export default function CodeTab({ repoName = '', id = '' }: Props) {
 
     if (!fileContent) return
 
-    setFileContent(fileContent)
+    console.log(fileObject)
+
+    setFileContent({ original: fileContent, modified: fileContent })
     setFilename(fileObject.path)
   }
 
   function onGoBackClick() {
-    setFileContent('')
+    setFileContent({ original: '', modified: '' })
     setFilename('')
+    setIsEditMode(false)
+    setIsPreviewMode(false)
+  }
+
+  async function handleCommitChangesClick() {
+    const blob = new Blob([fileContent.modified], { type: 'text/plain' })
+    const file = new File([blob], 'example.txt', { type: 'text/plain' })
+
+    console.log(file)
   }
 
   if (git.status === 'PENDING') {
@@ -88,7 +106,7 @@ export default function CodeTab({ repoName = '', id = '' }: Props) {
     return <RepoError />
   }
 
-  if (git.status === 'SUCCESS' && fileContent.length > 0) {
+  if (git.status === 'SUCCESS' && fileContent.original.length > 0) {
     trackGoogleAnalyticsEvent('Repository', 'Load a repo', 'Successfully loaded a repo', {
       repo_id: id,
       repo_name: repoName
@@ -98,38 +116,101 @@ export default function CodeTab({ repoName = '', id = '' }: Props) {
 
     return (
       <div className="flex flex-col gap-2 w-full h-full">
-        <div className="flex w-full justify-between">
+        <div className="flex w-full justify-between h-10">
           <Button onClick={onGoBackClick} className="gap-2 font-medium" variant="primary-outline">
             <FiArrowLeft className="w-5 h-5 text-[inherit]" /> Go back
           </Button>
-          {contributor && (
-            <div>
-              <Button className="gap-2 font-medium" variant="primary-solid">
-                <FiEdit3 className="w-5 h-5 text-[inherit]" />
-                Edit
+          {contributor && isEditMode && (
+            <div className="flex gap-2">
+              <Button
+                onClick={() => {
+                  setIsEditMode(false)
+                  setIsPreviewMode(false)
+                }}
+                variant="primary-outline"
+              >
+                Cancel changes
+              </Button>
+              <Button
+                onClick={handleCommitChangesClick}
+                variant="primary-solid"
+                disabled={fileContent.original === fileContent.modified}
+              >
+                Commit changes...
               </Button>
             </div>
           )}
         </div>
         <div className="flex w-full h-full mb-4">
           <div className="w-full flex flex-col border-gray-300 border-[1px] rounded-lg bg-white overflow-hidden">
-            <div className="rounded-t-lg flex justify-between bg-gray-200 border-b-[1px] border-gray-300 items-center gap-2 py-2 px-4 text-gray-900 font-medium">
-              {filename}
+            <div className="rounded-t-lg flex justify-between bg-gray-200 border-b-[1px] border-gray-300 items-center gap-2 py-2 px-4 text-gray-900 font-medium h-10">
+              <span className="order-2">{filename}</span>
+              {isEditMode ? (
+                <div className="flex gap-1 h-7 order-1">
+                  <Button
+                    onClick={() => setIsPreviewMode(false)}
+                    className={clsx(isPreviewMode && '!bg-[#E0E0E0] !border-0')}
+                    variant="primary-outline"
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    onClick={() => setIsPreviewMode(true)}
+                    className={clsx(!isPreviewMode && '!bg-[#E0E0E0] !border-0')}
+                    variant="primary-outline"
+                  >
+                    Preview
+                  </Button>
+                </div>
+              ) : (
+                <div
+                  onClick={() => setIsEditMode(true)}
+                  className="has-tooltip order-2 border border-gray-400 p-1 rounded-md cursor-pointer hover:bg-primary-200"
+                >
+                  <span className="tooltip rounded shadow-lg p-1 px-2 bg-gray-200 text-black text-sm -mt-12 -ml-12">
+                    Edit this file
+                  </span>
+                  <MdOutlineEdit className="h-4 w-4" />
+                </div>
+              )}
             </div>
             {isImage(filename) ? (
               <div className="h-full w-full bg-white flex items-center justify-center p-8">
-                <img src={fileContent} alt="Image" className="border border-gray-300 border-solid bg-[url('bg.gif')]" />
+                <img
+                  src={fileContent.original}
+                  alt="Image"
+                  className="border border-gray-300 border-solid bg-[url('bg.gif')]"
+                />
               </div>
+            ) : isPreviewMode ? (
+              <CodeMirrorMerge orientation="a-b" theme={githubLight}>
+                <CodeMirrorMerge.Original
+                  extensions={[
+                    langs.javascript({ jsx: true }),
+                    EditorView.editable.of(false),
+                    EditorState.readOnly.of(true)
+                  ]}
+                  value={fileContent.original}
+                />
+                <CodeMirrorMerge.Modified
+                  extensions={[
+                    langs.javascript({ jsx: true }),
+                    EditorView.editable.of(false),
+                    EditorState.readOnly.of(true)
+                  ]}
+                  value={fileContent.modified}
+                />
+              </CodeMirrorMerge>
             ) : (
               <CodeMirror
                 className="min-h-[100%] w-full"
-                value={fileContent}
+                value={isEditMode ? fileContent.modified : fileContent.original}
                 minHeight="200px"
                 height="100%"
                 theme={githubLight}
                 extensions={[langs.javascript({ jsx: true })]}
-                onChange={() => {}}
-                editable={false}
+                onChange={(value) => setFileContent((content) => ({ ...content, modified: value }))}
+                editable={isEditMode}
               />
             )}
           </div>
