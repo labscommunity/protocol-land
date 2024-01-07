8c22ef1db10627a93c2be741358847b66f534653
Pawan Paudel <pawanpaudel93@gmail.com>
2024-01-07 22:36:54 +0545

    fix: Allow setting user properties as empty string too

--- warp/protocol-land/actions/user.ts
+++ warp/protocol-land/actions/user.ts
@@ -21,15 +21,15 @@ export async function updateProfileDetails(
   // Validate each property of the payload against its expected type
   if (
     isInvalidInput(payload, 'object') ||
-    (payload.fullName !== undefined && isInvalidInput(payload.fullName, 'string')) ||
-    (payload.userName !== undefined && isInvalidInput(payload.userName, 'string')) ||
-    (payload.avatar !== undefined && isInvalidInput(payload.avatar, 'string')) ||
-    (payload.bio !== undefined && isInvalidInput(payload.bio, 'string')) ||
+    (payload.fullName !== undefined && isInvalidInput(payload.fullName, 'string', true)) ||
+    (payload.userName !== undefined && isInvalidInput(payload.userName, 'string', true)) ||
+    (payload.avatar !== undefined && isInvalidInput(payload.avatar, 'string', true)) ||
+    (payload.bio !== undefined && isInvalidInput(payload.bio, 'string', true)) ||
     (payload.timezone !== undefined && isInvalidTimezone(payload.timezone)) ||
-    (payload.location !== undefined && isInvalidInput(payload.location, 'string')) ||
-    (payload.twitter !== undefined && isInvalidInput(payload.twitter, 'string')) ||
-    (payload.email !== undefined && isInvalidInput(payload.email, 'string')) ||
-    (payload.website !== undefined && isInvalidInput(payload.website, 'string')) ||
+    (payload.location !== undefined && isInvalidInput(payload.location, 'string', true)) ||
+    (payload.twitter !== undefined && isInvalidInput(payload.twitter, 'string', true)) ||
+    (payload.email !== undefined && isInvalidInput(payload.email, 'string', true)) ||
+    (payload.website !== undefined && isInvalidInput(payload.website, 'string', true)) ||
     (payload.readmeTxId !== undefined && isInvalidInput(payload.readmeTxId, 'arweave-address'))
   ) {
     throw new ContractError('Invalid inputs supplied.')
