de4fae42b3e6fd2bd3eda42ba5bd6bcae1f6d361
Pawan Paudel <pawanpaudel93@gmail.com>
2024-01-08 10:24:14 +0545

    fix: Add missing box shadow to small circle

--- src/components/Landing/OpenSource.tsx
+++ src/components/Landing/OpenSource.tsx
@@ -21,9 +21,16 @@ export default function OpenSource() {
             Platform (Protocol.Land)
           </div>
         </div>
-        <div className="h-7 w-px lg:w-28 lg:h-px border-2 lg:border-t-0 border-l-0 border-dotted border-[#56ADD8]"></div>
-        <SVG className="hidden lg:block absolute" src={Line} />
-        <div className="w-1 h-1 bg-white rounded-full shadow blur-none"></div>
+        <div className="h-7 w-px lg:w-28 lg:h-px border-2 lg:border-t-0 border-l-0 border-dotted border-[#56ADD8] relative">
+          <SVG className="hidden lg:block absolute" src={Line} />
+          <div
+            className="hidden lg:block w-1 h-1 bg-white rounded-full shadow blur-none absolute right-2"
+            style={{
+              boxShadow: '0px 0px 3px 4px rgba(119, 198, 237, 0.5)'
+            }}
+          ></div>
+        </div>
+
         <div className="flex flex-col gap-2">
           <div className="h-80 bg-gray-50 rounded-lg shadow flex-col justify-start items-center flex drop-shadow-default">
             <img className="w-full h-full rounded-lg" src={PLViewblock} alt="" />
