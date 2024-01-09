98f940c4d804ea7c0511aba83114bf337c154bc3
Pawan Paudel <pawanpaudel93@gmail.com>
2024-01-09 12:54:33 +0545

    refactor: isInvalidInput function

--- src/helpers/isInvalidInput.ts
+++ src/helpers/isInvalidInput.ts
@@ -12,25 +12,25 @@ type ExpectedType =
   | 'email'
   | 'url'
 
-// https://github.com/uuidjs/uuid/blob/main/src/regex.js
-const UUID_REGEX =
-  /^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)$/i
+const regexPatterns = {
+  // https://github.com/uuidjs/uuid/blob/main/src/regex.js
+  uuid: /^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)$/i,
 
-const ADDRESS_REGEX = /^[a-z0-9-_]{43}$/i
+  address: /^[a-z0-9-_]{43}$/i,
 
-//  https://github.com/colinhacks/zod/blob/3e4f71e857e75da722bd7e735b6d657a70682df2/src/types.ts#L567C1-L568C86
-const EMAIL_REGEX = /^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i
+  //  https://github.com/colinhacks/zod/blob/3e4f71e857e75da722bd7e735b6d657a70682df2/src/types.ts#L567C1-L568C86
+  email: /^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i,
 
-// https://github.com/jquense/yup/blob/9e1df4938c1964a21e6ece0c458bb96dc5aff108/src/string.ts#L23C1-L25C1196
-const URL_REGEX =
-  /^((https?|ftp):)?\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
+  // https://github.com/jquense/yup/blob/9e1df4938c1964a21e6ece0c458bb96dc5aff108/src/string.ts#L23C1-L25C1196
+  url: /^((https?|ftp):)?\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
+}
 
 function isUuid(input: any): boolean {
-  return typeof input === 'string' && UUID_REGEX.test(input)
+  return typeof input === 'string' && regexPatterns.uuid.test(input)
 }
 
 function isArweaveAddress(input: any): boolean {
-  return typeof input === 'string' && ADDRESS_REGEX.test(input)
+  return typeof input === 'string' && regexPatterns.address.test(input)
 }
 
 function isObject(input: any) {
@@ -39,12 +39,31 @@ function isObject(input: any) {
 
 function isEmail(input: any, skipEmptyStringCheck: boolean) {
   if (skipEmptyStringCheck && input === '') return true
-  return typeof input === 'string' && EMAIL_REGEX.test(input)
+  return typeof input === 'string' && regexPatterns.email.test(input)
 }
 
 function isURL(input: any, skipEmptyStringCheck: boolean) {
   if (skipEmptyStringCheck && input === '') return true
-  return typeof input === 'string' && URL_REGEX.test(input)
+  return typeof input === 'string' && regexPatterns.url.test(input)
+}
+
+function isType(input: any, type: string, skipEmptyStringCheck: boolean): boolean {
+  switch (type) {
+    case 'object':
+      return isObject(input)
+    case 'array':
+      return Array.isArray(input)
+    case 'uuid':
+      return isUuid(input)
+    case 'arweave-address':
+      return isArweaveAddress(input)
+    case 'url':
+      return isURL(input, skipEmptyStringCheck)
+    case 'email':
+      return isEmail(input, skipEmptyStringCheck)
+    default:
+      return typeof input === type
+  }
 }
 
 /**
@@ -66,30 +85,5 @@ export const isInvalidInput = (
   }
 
   const typesToCheck = Array.isArray(expectedTypes) ? expectedTypes : [expectedTypes]
-
-  if (typesToCheck.includes('object')) {
-    return !isObject(input)
-  }
-
-  if (typesToCheck.includes('array')) {
-    return !Array.isArray(input)
-  }
-
-  if (typesToCheck.includes('uuid')) {
-    return !isUuid(input)
-  }
-
-  if (typesToCheck.includes('arweave-address')) {
-    return !isArweaveAddress(input)
-  }
-
-  if (typesToCheck.includes('url')) {
-    return !isURL(input, skipEmptyStringCheck)
-  }
-
-  if (typesToCheck.includes('email')) {
-    return !isEmail(input, skipEmptyStringCheck)
-  }
-
-  return !typesToCheck.some((expectedType) => typeof input === expectedType)
+  return !typesToCheck.some((expectedType) => isType(input, expectedType, skipEmptyStringCheck))
 }
