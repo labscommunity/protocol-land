d7b10610f51cdfa60d229fbfc0884f625383b15e
Pawan Paudel <pawanpaudel93@gmail.com>
2024-01-22 16:30:16 +0545

    Update commit row to show long username properly

--- src/components/CommitRow/index.tsx
+++ src/components/CommitRow/index.tsx
@@ -1,4 +1,5 @@
 import { formatDistanceToNow } from 'date-fns'
+import { Link } from 'react-router-dom'
 
 import { shortenAddress } from '@/helpers/shortenAddress'
 import { CommitResult } from '@/types/commit'
@@ -6,16 +7,19 @@ import { CommitResult } from '@/types/commit'
 export default function CommitRow({ commit }: { commit: CommitResult }) {
   return (
     <div className="flex justify-between flex-1 border-[1px] border-gray-300 bg-gray-200 rounded-lg w-full px-4 py-2">
-      <div className="flex gap-6">
-        <span className="text-gray-900">{shortenAddress(commit.commit.author.name)}</span>
-        <div className="w-[1px] h-full bg-gray-400" />
+      <div className="flex flex-col">
         <span className="text-gray-900 font-medium">{commit.commit.message}</span>
+        <div className="flex flex-row text-gray-600 hover:text-gray-900 gap-1">
+          <Link className="hover:underline" to={`/user/${commit.commit.author.name}`}>
+            {shortenAddress(commit.commit.author.name)}
+          </Link>
+          <span>
+            committed {formatDistanceToNow(new Date(commit.commit.committer.timestamp * 1000), { addSuffix: true })}
+          </span>
+        </div>
       </div>
       <div className="flex gap-6">
         <span className="text-gray-900">{commit.oid.slice(0, 7)}</span>
-        <span className="text-gray-900">
-          {formatDistanceToNow(new Date(commit.commit.committer.timestamp * 1000), { addSuffix: true })}
-        </span>
       </div>
     </div>
   )
