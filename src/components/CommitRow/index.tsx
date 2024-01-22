66e30a679444eefa85d521f2136cced80a6d60b0
Pawan Paudel <pawanpaudel93@gmail.com>
2024-01-22 20:55:59 +0545

    refactor: Update commits username and timestamp font weight

--- src/components/CommitRow/index.tsx
+++ src/components/CommitRow/index.tsx
@@ -9,12 +9,16 @@ export default function CommitRow({ commit }: { commit: CommitResult }) {
     <div className="flex justify-between flex-1 border-[1px] border-gray-300 bg-gray-200 rounded-lg w-full px-4 py-2">
       <div className="flex flex-col">
         <span className="text-gray-900 font-medium">{commit.commit.message}</span>
-        <div className="flex flex-row text-gray-600 hover:text-gray-900 gap-1">
-          <Link className="hover:underline" to={`/user/${commit.commit.author.name}`}>
+        <div className="inline-block text-gray-600 hover:text-gray-900">
+          <Link
+            className="hover:underline hover:text-primary-700 font-medium"
+            to={`/user/${commit.commit.author.name}`}
+          >
             {resolveUsernameOrShorten(commit.commit.author.name)}
           </Link>
-          <span>
-            committed {formatDistanceToNow(new Date(commit.commit.committer.timestamp * 1000), { addSuffix: true })}
+          <span> committed </span>
+          <span className="font-medium">
+            {formatDistanceToNow(new Date(commit.commit.committer.timestamp * 1000), { addSuffix: true })}
           </span>
         </div>
       </div>
