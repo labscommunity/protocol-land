157acd807ab4c7ab62648c64e62f09b17cd8cb03
Pawan Paudel <pawanpaudel93@gmail.com>
2024-01-25 18:34:10 +0545

    fix: Update Readme header height & border

--- src/pages/repository/components/tabs/code-tab/Readme.tsx
+++ src/pages/repository/components/tabs/code-tab/Readme.tsx
@@ -1,6 +1,8 @@
 import MDEditor from '@uiw/react-md-editor'
-import React, { useEffect } from 'react'
+import clsx from 'clsx'
+import React, { useEffect, useState } from 'react'
 import { LiaReadme } from 'react-icons/lia'
+import Sticky from 'react-stickynode'
 
 import { getFileContent } from '@/pages/repository/helpers/filenameHelper'
 import { useGlobalStore } from '@/stores/globalStore'
@@ -10,6 +12,7 @@ export default function Readme({ fileObject }: { fileObject?: FileObject }) {
   const [fileContent, setFileContent] = React.useState('')
   const [isLoading, setIsLoading] = React.useState(false)
   const [gitActions] = useGlobalStore((state) => [state.repoCoreActions.git])
+  const [isSticky, setIsSticky] = useState(false)
 
   useEffect(() => {
     loadReadmeContent()
@@ -32,14 +35,25 @@ export default function Readme({ fileObject }: { fileObject?: FileObject }) {
     setIsLoading(false)
   }
 
+  const handleStateChange = (status: Sticky.Status) => {
+    setIsSticky(status.status === Sticky.STATUS_FIXED)
+  }
+
   if (!fileObject) return null
 
   return (
     <div className="border border-gray-300 rounded-lg">
-      <div className="sticky -top-1 z-10 h-12 flex items-center gap-2 bg-gray-200 border-gray-300 border-b rounded-t-lg px-4">
-        <LiaReadme className="w-5 h-5" />
-        <div className="font-medium">README</div>
-      </div>
+      <Sticky top={0} innerActiveClass="z-10" onStateChange={handleStateChange}>
+        <div
+          className={clsx(
+            'z-10 h-[41px] flex items-center gap-2 bg-gray-200 border-gray-300 border-b px-4',
+            !isSticky && 'rounded-t-lg'
+          )}
+        >
+          <LiaReadme className="w-5 h-5" />
+          <div className="font-medium">README</div>
+        </div>
+      </Sticky>
       <MDEditor.Markdown className="p-8 !min-h-[200px] rounded-b-lg" source={isLoading ? 'Loading...' : fileContent} />
     </div>
   )
