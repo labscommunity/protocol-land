968ee61aff92eb657d7cd7999482d4678a798fc0
Sai Kranthi <iamsaikranthi@gmail.com>
2024-01-29 20:19:08 +0530

    refactor: use dispatch method to signAndSend from arconnect

--- src/helpers/wallet/signAndSend.ts
+++ src/helpers/wallet/signAndSend.ts
@@ -1,7 +1,30 @@
+import Arweave from 'arweave/web'
 import { Tag } from 'arweave/web/lib/transaction'
 import { createData } from 'warp-arbundles'
 
+import { useGlobalStore } from '@/stores/globalStore'
+
 export async function signAndSendTx(data: string | ArrayBuffer | Uint8Array, tags: Tag[], signer: any) {
+  const isArconnect = useGlobalStore.getState().authState.method === 'arconnect'
+
+  if (isArconnect) {
+    const arweave = new Arweave({
+      host: 'ar-io.net',
+      port: 443,
+      protocol: 'https'
+    })
+
+    const transaction = await arweave.createTransaction({
+      data
+    })
+
+    tags.forEach((tag) => transaction.addTag(tag.name, tag.value))
+
+    const dataTxResponse = await window.arweaveWallet.dispatch(transaction)
+
+    return dataTxResponse.id
+  }
+
   const node = 'https://turbo.ardrive.io'
 
   if (data instanceof ArrayBuffer) {
