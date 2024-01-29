4b4dda0e0530c56db3ae7bd65a379c5afdb3c023
Pawan Paudel <pawanpaudel93@gmail.com>
2024-01-29 14:41:38 +0545

    feat: Add find a repository input to search user repos

--- src/pages/profile/components/tabs/repositories/index.tsx
+++ src/pages/profile/components/tabs/repositories/index.tsx
@@ -1,3 +1,6 @@
+import clsx from 'clsx'
+import { useMemo, useState } from 'react'
+
 import { useGlobalStore } from '@/stores/globalStore'
 import { RepoWithParent } from '@/types/repository'
 
@@ -5,23 +8,41 @@ import RepoItem from './components/RepoItem'
 
 export default function RepositoriesTab({ userRepos }: { userRepos: RepoWithParent[] }) {
   const [connectedAddress] = useGlobalStore((state) => [state.authState.address])
+  const [searchTerm, setSearchTerm] = useState('')
+  const filteredRepos = useMemo(() => {
+    if (searchTerm && userRepos.length > 0) {
+      return userRepos.filter((repo) => repo.name.includes(searchTerm))
+    }
+    return userRepos
+  }, [userRepos, searchTerm])
 
   return (
-    <div className="flex flex-col w-full gap-3 mt-6">
-      {userRepos.map((repo) => {
-        const isContributor =
-          connectedAddress && (connectedAddress === repo.owner || repo.contributors.includes(connectedAddress))
-        if (repo.private && !isContributor) return null
-        return (
-          <RepoItem
-            id={repo.id}
-            title={repo.name}
-            description={repo.description}
-            parentRepo={repo.parentRepo}
-            isPrivate={repo.private}
-          />
-        )
-      })}
+    <div>
+      <input
+        type="text"
+        className={clsx(
+          'bg-white border-[1px] text-gray-900 text-base rounded-lg hover:shadow-[0px_2px_4px_0px_rgba(0,0,0,0.10)] focus:border-primary-500 focus:border-[1.5px] block w-full px-3 py-2 outline-none border-gray-300'
+        )}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Find a repository..."
+      />
+      <div className="flex flex-col w-full gap-3 mt-6">
+        {filteredRepos.map((repo) => {
+          const isContributor =
+            connectedAddress && (connectedAddress === repo.owner || repo.contributors.includes(connectedAddress))
+          if (repo.private && !isContributor) return null
+          return (
+            <RepoItem
+              id={repo.id}
+              title={repo.name}
+              description={repo.description}
+              parentRepo={repo.parentRepo}
+              isPrivate={repo.private}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
