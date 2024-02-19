2d374f7f6746b6f3546a5483d0f68032bd782bd4
Pawan Paudel <pawanpaudel93@gmail.com>
2024-02-19 18:37:37 +0545

    fix: Update folder icon stroke

--- src/pages/repository/components/tabs/code-tab/Row.tsx
+++ src/pages/repository/components/tabs/code-tab/Row.tsx
@@ -24,7 +24,7 @@ export default function Row({ isFolder, item, onFolderClick, onFileClick }: Prop
       onClick={handleRowClick}
       className="flex bg-gray-50 cursor-pointer hover:bg-primary-50 text-gray-600 hover:text-gray-900 items-center gap-4 py-[10px] px-4 border-b-[1px] border-gray-300 last:border-b-0"
     >
-      <SVG src={Icon} className={clsx('w-5 h-5', item.type === 'folder' && 'fill-primary-600 stroke-primary-600')} />{' '}
+      <SVG src={Icon} className={clsx('w-5 h-5', item.type === 'folder' && 'fill-primary-600 text-primary-700')} />{' '}
       <span>{item.path}</span>
     </div>
   )
