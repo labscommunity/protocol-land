c75eb58e1355cd6710f53154347fbc110146f6c4
Pawan Paudel <pawanpaudel93@gmail.com>
2024-03-04 14:23:45 +0545

    fix: Call sync state only once and wait for it to resolve

--- src/helpers/getWrapContract.ts
+++ src/helpers/getWrapContract.ts
@@ -6,19 +6,25 @@ const warp = WarpFactory.forMainnet().use(new DeployPlugin())
 const CACHE_URL = 'https://pl-cache.saikranthi.dev/contract'
 const IS_DRE_SYNCED_KEY = 'is_pl_dre_synced'
 let IS_DRE_SYNCED = false
+let syncPromise: Promise<void> | null = null
 
 export default async function getWarpContract(contractTxId: string, signer?: any) {
   const contract = warp.contract(contractTxId)
   IS_DRE_SYNCED = IS_DRE_SYNCED || localStorage.getItem(IS_DRE_SYNCED_KEY) === 'true'
 
   if (!IS_DRE_SYNCED) {
-    await contract
-      .syncState(CACHE_URL, { validity: true })
-      .catch((err: any) => console.log('DRE Sync Error: ', err?.message))
-      .finally(() => {
-        IS_DRE_SYNCED = true
-        localStorage.setItem(IS_DRE_SYNCED_KEY, 'true')
-      })
+    if (!syncPromise) {
+      syncPromise = contract
+        .syncState(CACHE_URL, { validity: true })
+        .catch((err: any) => console.log('DRE Sync Error: ', err?.message))
+        .finally(() => {
+          IS_DRE_SYNCED = true
+          localStorage.setItem(IS_DRE_SYNCED_KEY, 'true')
+          syncPromise = null
+        })
+    }
+
+    await syncPromise
   }
 
   if (signer) {
