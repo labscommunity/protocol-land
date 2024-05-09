5d89aa2e57e3b474f6e98851f6b7676fe661696f
Sai Kranthi <iamsaikranthi@gmail.com>
2024-05-09 19:23:37 +0530

    chore: bump arfs

--- src/lib/arfs/getArFS.ts
+++ src/lib/arfs/getArFS.ts
@@ -1,7 +1,7 @@
 import { ArFS } from 'arfs-js'
 
 export function getArFS() {
-  const arfs = new ArFS({ wallet: 'use_wallet' })
+  const arfs = new ArFS({ wallet: 'use_wallet', appName: 'Protocol.Land' })
 
   return arfs
 }
