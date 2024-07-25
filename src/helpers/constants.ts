4d14adbf5d9cfce99735e622b1374b4c1aa2ac26
Sai Kranthi <iamsaikranthi@gmail.com>
2024-07-25 11:07:00 +0530

    refactor: update hackathon handlers to work with ao

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,4 +1,4 @@
-export const AOS_PROCESS_ID = 'Mj6eddWvVjxROVUvUoz-ysr0lrcb36b9hNgF5NKLoLw'
+export const AOS_PROCESS_ID = 'yJZ3_Yrc-qYRt1zHmY7YeNvpmQwuqyK3dT0-gxWftew'
 export const VITE_GA_TRACKING_ID = 'G-L433HSR0D0'
 export const AMPLITUDE_TRACKING_ID = '92a463755ed8c8b96f0f2353a37b7b2'
 export const PL_REPO_ID = '6ace6247-d267-463d-b5bd-7e50d98c3693'
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import './index.css'
 
-import OthentStrategy from '@arweave-wallet-kit/othent-strategy'
 import ArConnectStrategy from '@arweave-wallet-kit-beta/arconnect-strategy'
 import BrowserWalletStrategy from '@arweave-wallet-kit-beta/browser-wallet-strategy'
 import { ArweaveWalletKit } from '@arweave-wallet-kit-beta/react'
@@ -15,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <HelmetProvider>
     <ArweaveWalletKit
       config={{
-        strategies: [new ArConnectStrategy(), new BrowserWalletStrategy(), new OthentStrategy()],
+        strategies: [new ArConnectStrategy(), new BrowserWalletStrategy()],
         permissions: [
           'ACCESS_ADDRESS',
           'SIGN_TRANSACTION',
