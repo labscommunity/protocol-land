cf9b37d9c67f79ae9867e24baf09128118f8c858
Sai Kranthi <iamsaikranthi@gmail.com>
2024-07-31 15:17:43 +0530

    fix: remove depricated vertical tab imports in settings page

--- src/pages/repository/components/tabs/settings-tab/index.tsx
+++ src/pages/repository/components/tabs/settings-tab/index.tsx
@@ -1,4 +1,4 @@
-import { Tab } from '@headlessui/react'
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 
 import { tabConfig } from './tabConfig'
 
@@ -7,10 +7,8 @@ const activeClasses = 'text-gray-900 font-medium'
 export default function SettingsTab() {
   return (
     <div className="w-full px-2 pb-6 flex gap-8 flex-1">
-      <Tab.Group vertical>
-        <Tab.List
-          className={'text-gray-500 text-lg border-r-[1px] border-gray-200 flex flex-col px-2 w-[16%]'}
-        >
+      <TabGroup className={'flex w-full'} vertical>
+        <TabList className={'text-gray-500 text-lg border-r-[1px] border-gray-200 flex flex-col px-2 w-[16%]'}>
           {tabConfig.map((tab) => (
             <Tab className="focus-visible:outline-none">
               {({ selected }) => (
@@ -18,15 +16,15 @@ export default function SettingsTab() {
               )}
             </Tab>
           ))}
-        </Tab.List>
-        <Tab.Panels className={'w-full'}>
+        </TabList>
+        <TabPanels className={'w-full'}>
           {tabConfig.map((TabContent) => (
-            <Tab.Panel className={'w-full'}>
+            <TabPanel className={'w-full px-8'}>
               <TabContent.Component />
-            </Tab.Panel>
+            </TabPanel>
           ))}
-        </Tab.Panels>
-      </Tab.Group>
+        </TabPanels>
+      </TabGroup>
     </div>
   )
 }
