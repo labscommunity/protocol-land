0a955f5e701317075f412135e6effa2750098c66
Sai Kranthi <iamsaikranthi@gmail.com>
2024-08-16 05:45:01 +0530

    feat: display forks with graphs

---
+++ src/pages/repository/components/tabs/forks-tab/RepoTreeMap.tsx
@@ -0,0 +1,37 @@
+import '@xyflow/react/dist/base.css'
+
+import { addEdge, Position, ReactFlow, useEdgesState, useNodesState, useReactFlow } from '@xyflow/react'
+import React from 'react'
+import { useCallback } from 'react'
+
+import { useGlobalStore } from '@/stores/globalStore'
+
+import { CustomNode } from './CustomNode'
+
+const nodeTypes = {
+  custom: CustomNode
+}
+
+export default function RepoTreeMap() {
+  const reactFlow = useReactFlow()
+  const [repoHierarchy] = useGlobalStore((state) => [state.repoCoreState.selectedRepo.repoHierarchy])
+  const [nodes, test, onNodesChange] = useNodesState(repoHierarchy.nodes)
+  const [edges, setEdges, onEdgesChange] = useEdgesState(repoHierarchy.edges)
+  const onConnect = useCallback((params: any) => setEdges((els) => addEdge(params, els)), [])
+  React.useEffect(() => {
+    reactFlow.setViewport({ x: 10, y: 200, zoom: 0.7 })
+  }, [reactFlow])
+  return (
+    <>
+      <ReactFlow
+        nodeTypes={nodeTypes}
+        className="!w-full !h-full flex flex-col flex-1"
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      />
+    </>
+  )
+}
