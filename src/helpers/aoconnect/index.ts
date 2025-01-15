60644ce34f9d2dda0aa08bde97d6e982758ae2b6
Sai Kranthi <iamsaikranthi@gmail.com>
2025-01-16 03:43:48 +0530

    feat: patch curves

--- src/helpers/aoconnect/index.ts
+++ src/helpers/aoconnect/index.ts
@@ -1,7 +1,7 @@
 import { connect, createDataItemSigner } from '@permaweb/aoconnect'
 
 const { result, results, message, spawn, monitor, unmonitor, dryrun } = connect({
-  GATEWAY_URL: 'https://g8way.io'
+  GATEWAY_URL: 'https://arweave-search.goldsky.com'
 })
 
 export { createDataItemSigner, dryrun, message, monitor, result, results, spawn, unmonitor }
